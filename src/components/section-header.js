76b2e77aa139241ef22532dc121af4d70e67400b
Baran Öden <baranoden@gmail.com>
2022-02-06 15:47:45 +0300

    add: Feature sections finished.

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { Box, Text, Heading } from 'theme-ui';
+import React from "react";
+import { Box, Text, Heading } from "theme-ui";
 
 export default function SectionHeader({ title, slogan, isWhite }) {
   return (
-<Box sx={{variant: "sectionHeader"}}>
-  <Text
-  sx={{variant: "sectionHeader.subTitle",
-   color: isWhite? "white" : "primary",
-  opacity: isWhite ? 0.7: 1,}}
-  
-  as="p">
-    {slogan}
-
-  </Text>
-  <Heading sx={{variant: "sectionHeader.title",
-   color: isWhite? "white" : "heading",
-  }}>
-    {title}
-
-  </Heading>
-</Box>  );
+    <Box sx={{ variant: "sectionHeader" }}>
+      <Text
+        sx={{
+          variant: "sectionHeader.subTitle",
+          color: isWhite ? "white" : "primary",
+          opacity: isWhite ? 0.7 : 1,
+        }}
+        as="p"
+      >
+        {slogan}
+      </Text>
+      <Heading
+        as="h2"
+        sx={{
+          variant: "sectionHeader.title",
+          color: isWhite ? "white" : "heading",
+        }}
+      >
+        {title}
+      </Heading>
+    </Box>
+  );
 }
